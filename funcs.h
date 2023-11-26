df1d13ac87a037c571c3237f346e90c20b7e084d
Yasherica <sava007.sava@yandex.ru>
2023-11-26 15:36:32 +0300

    add function MyDiv() and MyMul()

---
+++ funcs.h
@@ -0,0 +1,10 @@
+// заголовочный файл funcs.h
+#ifndef __FUNCS_H__
+#define __FUNCS_H__
+    
+double MySum(double, double);
+double MySub(double, double);
+double MyMu1(double, double);
+double MyDiv(double, double);
+
+#endif // __FUNCS_H__
